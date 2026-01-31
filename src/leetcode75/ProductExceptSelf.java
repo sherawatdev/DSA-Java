f67925328af064f62f4d017311af032b5c98842c
sherawatdev <devsherawat3011@gmail.com>
2026-01-31 16:51:18 +0530

    -update Product Except Self

--- src/leetcode75/ProductExceptSelf.java
+++ src/leetcode75/ProductExceptSelf.java
@@ -7,6 +7,7 @@ public class ProductExceptSelf {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4};
 //        int[] nums = {-1,1,0,-3,3};
+//        int[] nums = {2,3,0,0};
         int[] outArr = productExceptSelf(nums);
         System.out.println(Arrays.toString(outArr));
     }
@@ -15,15 +16,20 @@ public class ProductExceptSelf {
         int[] outArr = new int[nums.length];
         int product = 1;
         int productOfZero = 1;
+        int allZeroCount = 0;
         for (int i = 0; i< nums.length; i++) {
             int currentNum = nums[i];
             if (currentNum != 0) {
                 productOfZero = productOfZero * currentNum;
+            } else {
+                allZeroCount++;
             }
             product = product * currentNum;
         }
         for (int i = 0; i< nums.length; i++) {
-            if (nums[i] == 0) {
+            if (allZeroCount > 1) {
+                outArr[i] = 0;
+            } else if (nums[i] == 0) {
                 outArr[i] = productOfZero;
             } else {
                 outArr[i] = product/nums[i];
